17838cfb1df229bc96abf0dc731a39cf0de31619
Annmary <annmaryamaka@yahoo.com>
2021-02-10 09:06:57 +0100

    - rewrite readme file - remove unneccessary files

--- src/main/java/com/example/application/views/about/AboutView.java
+++ src/main/java/com/example/application/views/about/AboutView.java
@@ -3,18 +3,24 @@ package com.example.application.views.about;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.example.application.views.main.MainView;
+import com.vaadin.flow.router.RouteAlias;
 
-@Route(value = "about", layout = MainView.class)
-@PageTitle("About")
+@Route(value = "", layout = MainView.class)
+@PageTitle("Hacker News")
 @CssImport("./styles/views/about/about-view.css")
 public class AboutView extends Div {
 
     public AboutView() {
-        addClassName("about-view");
-        add(new Text("Content placeholder"));
+        addClassName("home");
+        Image image = new Image("https://volument.com/blog/img/hn-dirt-big.png", "DummyImage");
+        image.setWidthFull();
+        image.addClassName("hacker-image");
+        add(image);
+        add(new Text("News Contents"));
     }
 
 }
--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -22,25 +22,20 @@ import com.vaadin.flow.router.RouterLink;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import com.example.application.views.main.MainView;
-import com.example.application.views.helloworld.HelloWorldView;
 import com.example.application.views.about.AboutView;
 
 /**
  * The main view is a top-level placeholder for other views.
  */
 @CssImport("./styles/views/main/main-view.css")
-@PWA(name = "My Project", shortName = "My Project", enableInstallPrompt = false)
+@PWA(name = "Hacker News", shortName = "Hacker News", enableInstallPrompt = false)
 @JsModule("./styles/shared-styles.js")
 public class MainView extends AppLayout {
-
-    private final Tabs menu;
     private H1 viewTitle;
 
     public MainView() {
         setPrimarySection(Section.DRAWER);
         addToNavbar(true, createHeaderContent());
-        menu = createMenu();
-        addToDrawer(createDrawerContent(menu));
     }
 
     private Component createHeaderContent() {
@@ -57,53 +52,13 @@ public class MainView extends AppLayout {
         return layout;
     }
 
-    private Component createDrawerContent(Tabs menu) {
-        VerticalLayout layout = new VerticalLayout();
-        layout.setSizeFull();
-        layout.setPadding(false);
-        layout.setSpacing(false);
-        layout.getThemeList().set("spacing-s", true);
-        layout.setAlignItems(FlexComponent.Alignment.STRETCH);
-        HorizontalLayout logoLayout = new HorizontalLayout();
-        logoLayout.setId("logo");
-        logoLayout.setAlignItems(FlexComponent.Alignment.CENTER);
-        logoLayout.add(new Image("images/logo.png", "My Project logo"));
-        logoLayout.add(new H1("My Project"));
-        layout.add(logoLayout, menu);
-        return layout;
-    }
-
-    private Tabs createMenu() {
-        final Tabs tabs = new Tabs();
-        tabs.setOrientation(Tabs.Orientation.VERTICAL);
-        tabs.addThemeVariants(TabsVariant.LUMO_MINIMAL);
-        tabs.setId("tabs");
-        tabs.add(createMenuItems());
-        return tabs;
-    }
-
-    private Component[] createMenuItems() {
-        return new Tab[]{createTab("Hello World", HelloWorldView.class), createTab("About", AboutView.class)};
-    }
-
-    private static Tab createTab(String text, Class<? extends Component> navigationTarget) {
-        final Tab tab = new Tab();
-        tab.add(new RouterLink(text, navigationTarget));
-        ComponentUtil.setData(tab, Class.class, navigationTarget);
-        return tab;
-    }
-
     @Override
     protected void afterNavigation() {
         super.afterNavigation();
-        getTabForComponent(getContent()).ifPresent(menu::setSelectedTab);
+//        getTabForComponent(getContent()).ifPresent(menu::setSelectedTab);
         viewTitle.setText(getCurrentPageTitle());
     }
 
-    private Optional<Tab> getTabForComponent(Component component) {
-        return menu.getChildren().filter(tab -> ComponentUtil.getData(tab, Class.class).equals(component.getClass()))
-                .findFirst().map(Tab.class::cast);
-    }
 
     private String getCurrentPageTitle() {
         PageTitle title = getContent().getClass().getAnnotation(PageTitle.class);
